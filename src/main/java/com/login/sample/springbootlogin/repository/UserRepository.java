6bf88c2f48e843a885304f4305e9c8950fea49c5
Sewmini Dissanayaka <61307442+Sewmini-18@users.noreply.github.com>
2020-10-31 22:36:07 +0530

    backend login

--- src/main/java/com/login/sample/springbootlogin/repository/UserRepository.java
+++ src/main/java/com/login/sample/springbootlogin/repository/UserRepository.java
@@ -4,4 +4,6 @@ import com.login.sample.springbootlogin.document.Users;
 import org.springframework.data.mongodb.repository.MongoRepository;
 
 public interface UserRepository extends MongoRepository<Users, Long> {
+    Users findByEmail(String email);
+    Users findByEmailAndPassword(String email, String password);
 }
--- src/main/java/com/login/sample/springbootlogin/resource/UsersResource.java
+++ src/main/java/com/login/sample/springbootlogin/resource/UsersResource.java
@@ -11,18 +11,28 @@ import java.util.List;
 import java.util.Optional;
 
 @RestController
-//@RequestMapping("/home/users")
+
 @CrossOrigin(origins = "http://localhost:3000")
 public class UsersResource {
 
     @Autowired
     private UserRepository userRepository;
 
+
     @Autowired
     private SequenceGeneratorService sequenceGeneratorService;
 
     public UsersResource(UserRepository userRepository) {
         this.userRepository = userRepository;
+    }
+
+
+    public Users fetchUserByEmail(String email){
+        return userRepository.findByEmail(email);
+    }
+
+    public Users fetchUserByEmailAndPassword(String email, String password){
+        return userRepository.findByEmailAndPassword(email,password);
     }
 
     @GetMapping("/home/users")
@@ -42,4 +52,21 @@ public class UsersResource {
     public Optional<Users> findUser(@PathVariable long id){
         return userRepository.findById(id);
     }
+
+
+    @PostMapping("/login")
+    public Users loginUser(@RequestBody Users user) throws Exception {
+        String tempEmail = user.getEmail();
+        String tempPsw = user.getPassword();
+        Users userobj = null;
+        if (tempEmail != null && tempPsw != null) {
+            userobj = fetchUserByEmailAndPassword(tempEmail, tempPsw);
+        }
+        if(userobj==null){
+            throw new Exception("Bad Credintial");
+        }
+        return userobj;
+    }
+
+
 }
